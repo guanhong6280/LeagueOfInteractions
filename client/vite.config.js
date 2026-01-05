7967e9e356a7abf297bfd8ddd9a00407403305ac
guanhongjiang <guanhong6280@gmail.com>
2026-01-04 23:29:03 -0800

    added sourcemap for lighthouse to analyze.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     // No nested 'server' key here
     host: true, 
@@ -14,9 +17,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-    },
-    build: {
-      sourcemap: true,
     },
   },
 });
